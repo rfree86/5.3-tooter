1cc48e992394205994831e6910d3aefaad711cc7
Richard <rfree86@gmail.com>
2015-09-23 17:33:48 -0400

    done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,38 @@
 var models = require('models');
 var views = require('views');
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('body').prepend(JST.application());
+
+  $('.js-create-post-form').on('submit', function(event) {
+event.preventDefault();
+    $(document).trigger("create:post", [{title: "Cool", body: "Cool"}]);
+
+  });
+
 
   var view = new PostsView();
 
-  $(document).on('posts:fetched', function(event, posts){
+
+  $(document).on('posts:fetched', function(event, posts) {
     view.showPosts(posts);
   });
 
   Post.fetch();
+});
+
+$(document).on('create:post', function() {
+
+  var title = $('.js-create-post-form-title').val();
+
+  var body = $('.js-create-post-form-body').val();
+
+  $.ajax({
+    url: "http://tiny-lasagna-server.herokuapp.com/collections/posts",
+    type: 'POST',
+    data: {
+      title: title,
+      body: body
+    }
+  });
 });
---
+++ tests/create-test.js
@@ -0,0 +1,18 @@
+// var expect = chai.expect;
+// require('main');
+//
+// describe('create test form', function() {
+//   it('should trigger a create:post event on the document with the title and body', function(done) {
+//     $('js-.create-post-form-title').val("Post Title");
+//     $('js-.create-post-form-body').val("Post Body")
+//
+//     $(document).on("create:post", function(event, post) {
+//       expect(false).to.equal(true);
+//       done();
+//     });
+//     $(document).ready(function() {
+//       $('.js-create-post-form-submit').trigger('submit');
+//     });
+//
+//   });
+// });
